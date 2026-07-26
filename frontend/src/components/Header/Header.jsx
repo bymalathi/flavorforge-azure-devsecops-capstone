1df8802dba964e8f6cb5f4520110c3638499d135
shettymalathi113 <shettymalathi113@gmail.com>
2026-07-26 07:52:28 +0000

    feat(frontend): implement reusable navigation bar

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,7 +1,18 @@
+import "./Header.css";
+
 function Header() {
   return (
     <header>
-      <h2>FlavorForge</h2>
+      <nav>
+        <div className="logo">🍽️ FlavorForge</div>
+
+        <div className="nav-links">
+          <a href="#">Home</a>
+          <a href="#">Recipes</a>
+          <a href="#">About</a>
+          <a href="#">Contact</a>
+        </div>
+      </nav>
     </header>
   );
 }
