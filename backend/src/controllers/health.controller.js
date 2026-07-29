27da92ed43430b1c174ee6a9efbd5413278ba18b
shettymalathi113 <shettymalathi113@gmail.com>
2026-07-29 05:31:19 +0000

    Removed Encoding

--- backend/src/controllers/health.controller.js
+++ backend/src/controllers/health.controller.js
@@ -1,11 +1,11 @@
 const healthService = require("../services/health.service");
 
 function getHealth(req, res) {
-    console.log("➡️ Controller reached");
+    console.log("Controller reached");
 
     const health = healthService.getHealthStatus();
 
-    console.log("➡️ Sending response");
+    console.log("Sending response");
 
     res.status(200).json(health);
 }
